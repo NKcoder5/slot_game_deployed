b1e55b313e9f7bd9a2c1525baf5534fbb4bb6b5a
NkCoder5 <nandhakumar4115@gmail.com>
2025-06-26 12:16:44 +0530

    3rd commit

--- slot_game/settings.py
+++ slot_game/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-max(g=!ht&zh=iyc179@z^ackyzu2y0tp-e0qn@(sd%zo6(&)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
